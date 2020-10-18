864da4f0f87368b7ad8c292f39b2288de5bc52a3
Inesadhisti <67363076+Inesadhisti@users.noreply.github.com>
2020-10-18 23:54:16 +0700

    Update process-insert-absensi.php

--- system/gurumapel/process-insert-absensi.php
+++ system/gurumapel/process-insert-absensi.php
@@ -2,12 +2,12 @@
 //panggil file conn.php untuk menghubung ke server
 include "system/config/conn.php";
 
-$nm_kelas=$_POST['nm_kelas'];
-$tanggal=$_POST['tanggal'];
+FILTER_INPUT(INPUT_GET, 'nm_kelas');
+FILTER_INPUT(INPUT_GET, 'tanggal');
 
 if(isset($_POST['info'])){
 	if($_POST['jam_pelajaran'] != "*Diluar Jam Pelajaran*"){
-		$jam_pelajaran=$_POST['jam_pelajaran'];
+		FILTER_INPUT(INPUT_GET, 'jam_pelajaran');
 
 		$query=mysql_query("SELECT nis FROM siswa WHERE nm_kelas='$nm_kelas' ORDER BY nis ASC");
 		while($data=mysql_fetch_array($query)){
@@ -17,7 +17,7 @@ if(isset($_POST['info'])){
 				$keterangan = $_POST['keterangan-'.$data['nis']];
 				$hadir=mysql_query("INSERT INTO absensi(nis,nm_kelas,ket,keterangan,tanggal,info,jam_pelajaran) VALUES ('$data[nis]','$nm_kelas','H','$keterangan','$tanggal','succes','$jam_pelajaran')",$connect);
 				?>
-					<script language="javascript">window.location.href="page.php?data-absensi&kelas=<?php echo $nm_kelas;?>&tanggal=<?php echo $tanggal;?>&message=absen-success";</script>
+					<script language="javascript">window.location.href="page.php?data-absensi&kelas=<?php <?= $nm_kelas >?;?>&tanggal=<?php <?= $tanggal >?;?>&message=absen-success";</script>
 				<?php 
 			}
 			else if($_POST['absen-'.$data['nis']] == 'sakit'){
@@ -25,7 +25,7 @@ if(isset($_POST['info'])){
 				$keterangan = $_POST['keterangan-'.$data['nis']];
 				$hadir=mysql_query("INSERT INTO absensi(nis,nm_kelas,ket,keterangan,tanggal,info,jam_pelajaran) VALUES ('$data[nis]','$nm_kelas','S','$keterangan','$tanggal','succes','$jam_pelajaran')",$connect);
 				?>
-				<script language="javascript">window.location.href="page.php?data-absensi&kelas=<?php echo $nm_kelas;?>&tanggal=<?php echo $tanggal;?>&message=absen-success";</script>
+				<script language="javascript">window.location.href="page.php?data-absensi&kelas=<?php <?= $nm_kelas >?;?>&tanggal=<?php <?= $tanggal >?;?>&message=absen-success";</script>
 				<?php 
 			}
 			else if($_POST['absen-'.$data['nis']] == 'ijin'){
@@ -33,7 +33,7 @@ if(isset($_POST['info'])){
 				$keterangan = $_POST['keterangan-'.$data['nis']];
 				$hadir=mysql_query("INSERT INTO absensi(nis,nm_kelas,ket,keterangan,tanggal,info,jam_pelajaran) VALUES ('$data[nis]','$nm_kelas','I','$keterangan','$tanggal','succes','$jam_pelajaran')",$connect);
 				?>
-				<script language="javascript">window.location.href="page.php?data-absensi&kelas=<?php echo $nm_kelas;?>&tanggal=<?php echo $tanggal;?>&message=absen-success";</script>
+				<script language="javascript">window.location.href="page.php?data-absensi&kelas=<?php <?= $nm_kelas >?;?>&tanggal=<?php <?= $tanggal >?;?>&message=absen-success";</script>
 				<?php 
 			}
 			else if($_POST['absen-'.$data['nis']] == 'alfa'){
@@ -41,7 +41,7 @@ if(isset($_POST['info'])){
 				$keterangan = $_POST['keterangan-'.$data['nis']];
 				$alfa=mysql_query("INSERT INTO absensi(nis,nm_kelas,ket,keterangan,tanggal,info,jam_pelajaran) VALUES ('$data[nis]','$nm_kelas','A','$keterangan','$tanggal','succes','$jam_pelajaran')",$connect);
 				?>
-				<script language="javascript">window.location.href="page.php?data-absensi&kelas=<?php echo $nm_kelas;?>&tanggal=<?php echo $tanggal;?>&message=absen-success";</script>
+				<script language="javascript">window.location.href="page.php?data-absensi&kelas=<?php <?= $nm_kelas >?;?>&tanggal=<?php <?= $tanggal >?;?>&message=absen-success";</script>
 				<?php 
 			}
 		}
@@ -49,11 +49,11 @@ if(isset($_POST['info'])){
 		unset($_POST['info']);
 		?><script language="javascript">
 			alert("Tidak dapat absen diluar jam pelajaran!");
-			window.location.href="page.php?absen-siswa&kelas=<?php echo $nm_kelas;?>";
+			window.location.href="page.php?absen-siswa&kelas=<?php <?= $nm_kelas >?;?>";
 		</script><?php
 	}
 }else{
 	unset($_POST['info']);
-	?><script language="javascript">window.location.href="page.php?absen-siswa&kelas=<?php echo $nm_kelas;?>";</script><?php
+	?><script language="javascript">window.location.href="page.php?absen-siswa&kelas=<?php <?= $nm_kelas >?;?>";</script><?php
 }
 ?>
