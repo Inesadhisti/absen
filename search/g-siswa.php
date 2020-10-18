cf21252895574ee7fa8f2e7645e0d02608ca4b54
Inesadhisti <67363076+Inesadhisti@users.noreply.github.com>
2020-10-18 15:13:33 +0700

    Update g-siswa.php

--- search/g-siswa.php
+++ search/g-siswa.php
@@ -6,7 +6,7 @@ include('../system/config/conn.php');
 //panggil file header.php untuk menghubungkan konten bagian atas
 include('inc/header.php');
 //memberi judul halaman
-	echo '<title>Pencarian Data Siswa - MARI-ABSEN</title>';
+	<?= '<title>Pencarian Data Siswa - MARI-ABSEN</title>' >?;
 //panggil file css.php untuk desain atau tema
 include('inc/css.php');
 //panggil file navi-admin.php untuk menghubungkan navigasi admin ke konten
@@ -65,14 +65,14 @@ include('inc/nav-gurumapel.php');
 								{
  								?>
 								<tr>
-								<td><?php echo $data['nama']; ?></td>
-								<td class="color-blue-grey-lighter"><?php echo $data['nis']; ?></td>
-								<td align="center"><?php echo $data['nm_kelas']; ?></td>
-								<td ><?php echo $data['jns_kel']; ?></td>
-								<td class="color-blue-grey-lighter"><?php echo $data['alamat']; ?></td>
+								<td><?php <?= $data['nama'] >?; ?></td>
+								<td class="color-blue-grey-lighter"><?php <?= $data['nis'] >?; ?></td>
+								<td align="center"><?php <?= $data['nm_kelas'] >?; ?></td>
+								<td ><?php <?= $data['jns_kel'] >?; ?></td>
+								<td class="color-blue-grey-lighter"><?php <?= $data['alamat'] >?; ?></td>
 								<td align="center">
 									<div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
-										<a href="../page.php?g-detail-siswa&id=<?php echo $data['id_siswa'];?>" class="btn btn btn-default" data-toggle="tooltip" data-placement="top" title="Detail?"><i class="font-icon font-icon-eye"></i> </a>
+										<a href="../page.php?g-detail-siswa&id=<?php <?= $data['id_siswa'] >?;?>" class="btn btn btn-default" data-toggle="tooltip" data-placement="top" title="Detail?"><i class="font-icon font-icon-eye"></i> </a>
 									</div>
 								</td>
 								</tr>
@@ -80,7 +80,7 @@ include('inc/nav-gurumapel.php');
 								} 
 								?>
 							</tbody>
-							<?php }else{ echo '<tr><td  colspan="7" align="center">NIS atau Nama Siswa tidak ditemukan!</td></tr>'; } } ?>
+							<?php }else{ <?= '<tr><td  colspan="7" align="center">NIS atau Nama Siswa tidak ditemukan!</td></tr>' >?; } } ?>
 						</table>
 					</div>
 				</div><!--.box-typical-body-->
@@ -89,7 +89,7 @@ include('inc/nav-gurumapel.php');
 					<div class="col-md-6">
 						<br>
   						<span class="label label-success">Info! </span> Hasil  Pencarian
-  						<span class="label label-primary">Untuk : <?php echo $q; ?> </span>
+  						<span class="label label-primary">Untuk : <?php <?= $q >?; ?> </span>
 					</div>
 					
 					<div class="col-md-6" align="right">
