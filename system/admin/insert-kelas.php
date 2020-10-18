49feb2c5072a4e07e894d8b899462c0d094b5abc
Inesadhisti <67363076+Inesadhisti@users.noreply.github.com>
2020-10-18 23:21:55 +0700

    Update insert-kelas.php

--- system/admin/insert-kelas.php
+++ system/admin/insert-kelas.php
@@ -49,5 +49,5 @@ include('system/inc/nav-admin.php');
 
 <?php 
 //panggil file footer.php untuk menghubungkan konten bagian bawah
-print ('system/inc/footer.php');
+include ('system/inc/footer.php');
 ?>
