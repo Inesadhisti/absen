e4f1c279e6aeaea8b4f6482b3157a8c6b7f3b86e
Inesadhisti <67363076+Inesadhisti@users.noreply.github.com>
2020-10-19 01:09:49 +0700

    Update nav-gurumapel.php

--- system/inc/nav-gurumapel.php
+++ system/inc/nav-gurumapel.php
@@ -22,7 +22,7 @@
 	           				<img src="assets/img/gurumapel.png" alt"">	
 	                        </button>
 	                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dd-user-menu">
-	                            <a class="dropdown-item" href="page.php?g-detail-profil&id=<?php <?= $_SESSION[id_user] >?;?>"><span class="font-icon glyphicon glyphicon-user"></span>Profil</a>
+	                            <a class="dropdown-item" href="page.php?g-detail-profil&id=<?php <?= FILTER_INPUT(INPUT_SESSION, 'id_user') >?;?>"><span class="font-icon glyphicon glyphicon-user"></span>Profil</a>
 								<div class="dropdown-divider"></div>
 	                            <a class="dropdown-item" href="page.php?keluar" onClick="return confirm('Yakin anda akan keluar?');"><span class="font-icon glyphicon glyphicon-log-out"></span>Keluar</a>
 	                        </div>
