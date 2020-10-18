19bf66d8fb2a493182d16664a4839f8ba08eeb3d
Inesadhisti <67363076+Inesadhisti@users.noreply.github.com>
2020-10-18 23:09:21 +0700

    Update process-insert-absensi-sholat.php

--- system/gurumapel/process-insert-absensi-sholat.php
+++ system/gurumapel/process-insert-absensi-sholat.php
@@ -1,10 +1,10 @@
 <?php 
 //panggil file conn.php untuk menghubung ke server
 include "system/config/conn.php";
-$nm_kelas=$_POST['nm_kelas'];
-$tanggal=$_POST['tanggal'];
+FILTER_INPUT(INPUT_POST, 'nm_kelas');
+FILTER_INPUT(INPUT_POST, 'tanggal');
 
-echo 'test';
+<?= 'test' >?;
 
 if(isset($_POST['info'])){
 	$query=mysql_query("SELECT nis FROM siswa WHERE nm_kelas='$nm_kelas' ORDER BY nis ASC");
@@ -15,7 +15,7 @@ if(isset($_POST['info'])){
 			$keterangan = $_POST['keterangan-'.$data['nis']];
 			$sholat=mysql_query("INSERT INTO absensi_sholat(nis,nm_kelas,ket,keterangan,tanggal,info) VALUES ('$data[nis]','$nm_kelas','S','$keterangan','$tanggal','succes')",$connect);
 			?>
-				<script language="javascript">window.location.href="page.php?data-absensi-sholat&kelas=<?php echo $nm_kelas;?>&tanggal=<?php echo $tanggal;?>&message=absen-success";</script>
+				<script language="javascript">window.location.href="page.php?data-absensi-sholat&kelas=<?php <?= $nm_kelas >?;?>&tanggal=<?php <?= $tanggal >?;?>&message=absen-success";</script>
 			<?php 
 		}
 		else if($_POST['absen-'.$data['nis']] == 'tidaksholat'){
@@ -23,7 +23,7 @@ if(isset($_POST['info'])){
 			$keterangan = $_POST['keterangan-'.$data['nis']];
 			$sholat=mysql_query("INSERT INTO absensi_sholat(nis,nm_kelas,ket,keterangan,tanggal,info) VALUES ('$data[nis]','$nm_kelas','TS','$keterangan','$tanggal','succes')",$connect);
 			?>
-			<script language="javascript">window.location.href="page.php?data-absensi-sholat&kelas=<?php echo $nm_kelas;?>&tanggal=<?php echo $tanggal;?>&message=absen-success";</script>
+			<script language="javascript">window.location.href="page.php?data-absensi-sholat&kelas=<?php <?= $nm_kelas >?;?>&tanggal=<?php <?= $tanggal >?;?>&message=absen-success";</script>
 			<?php 
 		}
 		else if($_POST['absen-'.$data['nis']] == 'halangan'){
@@ -31,12 +31,12 @@ if(isset($_POST['info'])){
 			$keterangan = $_POST['keterangan-'.$data['nis']];
 			$sholat=mysql_query("INSERT INTO absensi_sholat(nis,nm_kelas,ket,keterangan,tanggal,info) VALUES ('$data[nis]','$nm_kelas','HL','$keterangan','$tanggal','succes')",$connect);
 			?>
-			<script language="javascript">window.location.href="page.php?data-absensi-sholat&kelas=<?php echo $nm_kelas;?>&tanggal=<?php echo $tanggal;?>&message=absen-success";</script>
+			<script language="javascript">window.location.href="page.php?data-absensi-sholat&kelas=<?php <?= $nm_kelas >?;?>&tanggal=<?php <?= $tanggal >?;?>&message=absen-success";</script>
 			<?php 
 		}
 	}
 }else{
 	unset($_POST['info']);
-	?><script language="javascript">window.location.href="page.php?absen-sholat&kelas=<?php echo $nm_kelas;?>";</script><?php
+	?><script language="javascript">window.location.href="page.php?absen-sholat&kelas=<?php <?= $nm_kelas >?;?>";</script><?php
 }
 ?>
