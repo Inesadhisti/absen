eea2f04e2459c35f78f281fe82efc3ebbe211076
Inesadhisti <67363076+Inesadhisti@users.noreply.github.com>
2020-10-19 01:38:45 +0700

    Update process-insert-kelas.php

--- system/admin/process-insert-kelas.php
+++ system/admin/process-insert-kelas.php
@@ -2,14 +2,14 @@
 //panggil file config.php untuk menghubung ke server
 include('system/config/conn.php');
 //tangkap data dari form
-$nm_kelas = $_POST['nm_kelas'];
+FILTER_INPUT(INPUT_POST, 'nm_kelas');
 
 //menghindari duplikat nama kelas
 $cek="SELECT nm_kelas FROM kelas WHERE nm_kelas='$nm_kelas'";
 $ada=mysql_query($cek) or die (mysql_error());
 if(mysql_num_rows($ada)>0)
 {
-	echo "<script>alert ('Nama Kelas Telah Terdaftar ! Silahkan Periksa Kembali !');window.location='page.php?tambah-kelas' </script> ";
+	<?= "<script>alert ('Nama Kelas Telah Terdaftar ! Silahkan Periksa Kembali !');window.location='page.php?tambah-kelas' </script> " >?;
 	}  
 
 //simpan data ke database
