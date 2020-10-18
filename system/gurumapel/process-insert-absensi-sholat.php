e3d2b16570276250b18fbb5c0cd734c98495c0fa
Inesadhisti <67363076+Inesadhisti@users.noreply.github.com>
2020-10-19 01:29:52 +0700

    Update process-insert-absensi-sholat.php

--- system/gurumapel/process-insert-absensi-sholat.php
+++ system/gurumapel/process-insert-absensi-sholat.php
@@ -12,23 +12,23 @@ if(isset(FILTER_INPUT(INPUT_POST, 'info'))){
 		mysql_query("DELETE FROM absensi_sholat WHERE nis='$data[nis]' AND tanggal='$tanggal'");
 		if(FILTER_INPUT(INPUT_POST, ''absen-'.$data['nis']') == 'sholat'){
 			//parameter
-			$keterangan = $_POST['keterangan-'.$data['nis']];
+			FILTER_INPUT(INPUT_POST, ''keterangan-'.$data['nis']');
 			$sholat=mysql_query("INSERT INTO absensi_sholat(nis,nm_kelas,ket,keterangan,tanggal,info) VALUES ('$data[nis]','$nm_kelas','S','$keterangan','$tanggal','succes')",$connect);
 			?>
 				<script language="javascript">window.location.href="page.php?data-absensi-sholat&kelas=<?php <?= $nm_kelas >?;?>&tanggal=<?php <?= $tanggal >?;?>&message=absen-success";</script>
 			<?php 
 		}
-		else if($_POST['absen-'.$data['nis']] == 'tidaksholat'){
+		else if(FILTER_INPUT(INPUT_POST, ''absen-'.$data['nis']') == 'tidaksholat'){
 			//parameter
-			$keterangan = $_POST['keterangan-'.$data['nis']];
+			FILTER_INPUT(INPUT_POST, ''keterangan-'.$data['nis']');
 			$sholat=mysql_query("INSERT INTO absensi_sholat(nis,nm_kelas,ket,keterangan,tanggal,info) VALUES ('$data[nis]','$nm_kelas','TS','$keterangan','$tanggal','succes')",$connect);
 			?>
 			<script language="javascript">window.location.href="page.php?data-absensi-sholat&kelas=<?php <?= $nm_kelas >?;?>&tanggal=<?php <?= $tanggal >?;?>&message=absen-success";</script>
 			<?php 
 		}
-		else if($_POST['absen-'.$data['nis']] == 'halangan'){
+		else if(FILTER_INPUT(INPUT_POST, ''absen-'.$data['nis']') == 'halangan'){
 			//parameter
-			$keterangan = $_POST['keterangan-'.$data['nis']];
+			FILTER_INPUT(INPUT_POST, ''keterangan-'.$data['nis']');
 			$sholat=mysql_query("INSERT INTO absensi_sholat(nis,nm_kelas,ket,keterangan,tanggal,info) VALUES ('$data[nis]','$nm_kelas','HL','$keterangan','$tanggal','succes')",$connect);
 			?>
 			<script language="javascript">window.location.href="page.php?data-absensi-sholat&kelas=<?php <?= $nm_kelas >?;?>&tanggal=<?php <?= $tanggal >?;?>&message=absen-success";</script>
@@ -36,7 +36,7 @@ if(isset(FILTER_INPUT(INPUT_POST, 'info'))){
 		}
 	}
 }else{
-	unset($_POST['info']);
+	unset(FILTER_INPUT(INPUT_POST, 'INFO');
 	?><script language="javascript">window.location.href="page.php?absen-sholat&kelas=<?php <?= $nm_kelas >?;?>";</script><?php
 }
 ?>
