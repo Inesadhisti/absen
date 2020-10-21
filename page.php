e30ecb39f5633f515fc00efa9b0803f28fca416c
Inesadhisti <67363076+Inesadhisti@users.noreply.github.com>
2020-10-22 01:55:15 +0700

    Update page.php

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 error_reporting(0);
 //halaman awal
  if(isset(FILTER_INPUT(INPUT_GET, 'masuk');)){ 
-include './sign-in.php';
+$this->load->model('./sign-out.php');
 }
  if(isset(FILTER_INPUT(INPUT_GET, 'keluar');)){ 
 include 'sign-out.php';
