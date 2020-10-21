734634cd07c2e9f4d32a57bfadad27c36ce403e1
Inesadhisti <67363076+Inesadhisti@users.noreply.github.com>
2020-10-21 22:05:36 +0700

    Update delete-user.php

--- system/admin/delete-user.php
+++ system/admin/delete-user.php
@@ -1,8 +1,9 @@
 <?php
 //panggil file conn.php untuk menghubung ke server
 include('system/config/conn.php');
-(FILTER_INPUT(INPUT_GET, 'id')
-$query = mysql_query("DELETE FROM user WHERE id_user='$id_user'");
+(FILTER_INPUT(INPUT_GET, 'id');
+$this->db->where('$id_user');
+$this->db->delete('user');
 if ($query) {
 header('location:page.php?data-user&message=delete-success');
 }
