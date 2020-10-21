ced68c833ff7870f35f72606ef0b2c44874833ed
Inesadhisti <67363076+Inesadhisti@users.noreply.github.com>
2020-10-22 00:17:08 +0700

    Update process-edit-kelas.php

--- system/admin/process-edit-kelas.php
+++ system/admin/process-edit-kelas.php
@@ -7,8 +7,9 @@ include('system/config/conn.php');
 (FILTER_INPUT(INPUT_POST, 'nm_kelas')
 
 //update data di database sesuai kd_kelas
-$query = mysql_query("update kelas set nm_kelas='$nm_kelas' where id_kelas='$id_kelas'");
-
+$this->db->set('nm_kelas', '$nm_kelas');
+$this->db->where('id_kelas', '$id_kelas');
+$query->update();
 if ($query) {
 	header('location:page.php?data-kelas&message=edit-success');
 }
