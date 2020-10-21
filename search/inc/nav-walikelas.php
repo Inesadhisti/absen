f5aed082fc6c11ed0b89e390450a6a0208ef0b58
Inesadhisti <67363076+Inesadhisti@users.noreply.github.com>
2020-10-21 23:52:46 +0700

    Update nav-walikelas.php

--- search/inc/nav-walikelas.php
+++ search/inc/nav-walikelas.php
@@ -82,7 +82,6 @@
 	            <ul>
 	            <li>
 				<?php 
-				$query=mysql_query("SELECT * FROM kelas ORDER BY nm_kelas ASC",$connect);
 			    	$this->db->from('kelas');
 			    	$this->db->order_by('nm_kelas');
 			    	$query->get();
