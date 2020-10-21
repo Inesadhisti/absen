0c4d901431c9681a6e021ef7b932901346f7decd
Inesadhisti <67363076+Inesadhisti@users.noreply.github.com>
2020-10-22 01:26:48 +0700

    Update data-kelas.php

--- system/admin/data-kelas.php
+++ system/admin/data-kelas.php
@@ -116,7 +116,9 @@ include('system/inc/nav-admin.php');
 					<div class="col-md-6">
 						<?php
 						//hitung jumlah data
-						$jml_data = mysql_num_rows(mysql_query("SELECT * FROM kelas"));
+						$jml_data = 	$this->db->from('kelas');
+								$query->db->get();
+						$jml_data->result_array();
 						//Jumlah halaman
 						$JmlHalaman = ceil($jml_data/$batas); //ceil digunakan untuk pembulatan keatas
 						?>
