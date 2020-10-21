ec6c50af14d42a65781ce221bd039a6505a7c68c
Inesadhisti <67363076+Inesadhisti@users.noreply.github.com>
2020-10-21 23:46:04 +0700

    Update delete-kelas.php

--- system/admin/delete-kelas.php
+++ system/admin/delete-kelas.php
@@ -2,7 +2,9 @@
 //panggil file conn.php untuk menghubung ke server
 include('system/config/conn.php');
 FILTER_INPUT(INPUT_GET, 'id');
-$query = mysql_query("DELETE FROM kelas WHERE id_kelas='$id_kelas'");
+$this->db->from('kelas');
+$this->db->where('id_kelas', '$id_kelas');
+$query->delete();
 if ($query) {
 header('location:page.php?data-kelas&message=delete-success');
 }
