fa7c180e834e24e8225ae6bad774cf50b0fff6e1
Inesadhisti <67363076+Inesadhisti@users.noreply.github.com>
2020-10-22 00:22:55 +0700

    Update delete-siswa.php

--- system/admin/delete-siswa.php
+++ system/admin/delete-siswa.php
@@ -2,7 +2,9 @@
 //panggil file conn.php untuk menghubung ke server
 include('system/config/conn.php');
 FILTER_INPUT(INPUT_GET, 'id')
-$query = mysql_query("DELETE FROM siswa WHERE id_siswa='$id_siswa'");
+$this->db->from('siswa');
+$this->db->where('id_siswa', '$id_siswa');
+$query->delete();
 if ($query) {
 header('location:page.php?data-semua-siswa&message=delete-success');
 }
