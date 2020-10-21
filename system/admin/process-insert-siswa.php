547082544c4fd3fb45545e7908d278ff7bd8ade0
Inesadhisti <67363076+Inesadhisti@users.noreply.github.com>
2020-10-22 01:31:45 +0700

    Update process-insert-siswa.php

--- system/admin/process-insert-siswa.php
+++ system/admin/process-insert-siswa.php
@@ -6,19 +6,21 @@ include('system/config/conn.php');
 $folder="system/images/"; 
 
 //tangkap data dari form
-FILTER_INPUT(INPUT_POST, 'nama')
-FILTER_INPUT(INPUT_POST, 'nis')
-FILTER_INPUT(INPUT_POST, 'nm_kelas')
-FILTER_INPUT(INPUT_POST, 'jns_kel')
-FILTER_INPUT(INPUT_POST, 'alamat')
-FILTER_INPUT(INPUT_POST, 'tmpt_lahir')
-FILTER_INPUT(INPUT_POST, 'tgl_lahir')
-FILTER_INPUT(INPUT_POST, 'nama_ortu')
+FILTER_INPUT(INPUT_POST, 'nama');
+FILTER_INPUT(INPUT_POST, 'nis');
+FILTER_INPUT(INPUT_POST, 'nm_kelas');
+FILTER_INPUT(INPUT_POST, 'jns_kel');
+FILTER_INPUT(INPUT_POST, 'alamat');
+FILTER_INPUT(INPUT_POST, 'tmpt_lahir');
+FILTER_INPUT(INPUT_POST, 'tgl_lahir');
+FILTER_INPUT(INPUT_POST, 'nama_ortu');
 
 //menghindari duplikat nis
-$cek="SELECT nis FROM siswa WHERE nis='$nis'";
-$ada=mysql_query($cek);
-if(mysql_num_rows($ada)>0)
+$this->db->from('siswa');
+$this->db->where('nis', '$nis');
+$cek= $this->db->get();
+$ada=$cek->result_array();
+if($ada->result_array()>0)
 {
 	<?= "<script>alert ('NIS Telah Terdaftar ! Silahkan Periksa Kembali !');window.location='page.php?tambah-siswa' </script> " >?;
 	}
